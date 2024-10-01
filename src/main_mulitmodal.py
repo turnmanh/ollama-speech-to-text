837e7c0911bb217e8814c44b62ec022e69c79961
turnmanh <17703667+turnmanh@users.noreply.github.com>
2024-10-01 18:48:01 +0200

    added more timing

--- src/main_mulitmodal.py
+++ src/main_mulitmodal.py
@@ -15,8 +15,15 @@ from prompt_utils import get_prompt
 from voice_utils import record_audio, transcribe
 
 
-def main(chain, image_b64: str, console: Console, speech_to_text: whisper.Whisper):
+def main(
+    chain,
+    image_b64: str,
+    console: Console,
+    speech_to_text: whisper.Whisper,
+    timings: bool = False,
+):
     console.print("[cyan]Assistant started! Press Ctrl+C to exit.")
+    timing = {}
 
     try:
         while True:
@@ -43,7 +50,11 @@ def main(chain, image_b64: str, console: Console, speech_to_text: whisper.Whispe
 
             if audio_np.size > 0:
                 with console.status("Transcribing...", spinner="earth"):
+                    # Time the transcription process.
+                    transcribe_start_time = time.time()
                     text = transcribe(audio_np, speech_to_text=speech_to_text)
+                    transcribe_end_time = time.time()
+                    timing["transcribe"] = transcribe_end_time - transcribe_start_time
 
                     # Fallback text if no transcription is available.
                     if len(text) == 0:
@@ -56,9 +67,12 @@ def main(chain, image_b64: str, console: Console, speech_to_text: whisper.Whispe
                 console.print(f"[yellow]You: {text}")
 
                 with console.status("Generating response...", spinner="earth"):
+                    # Time the response generation process.
                     query_start_time = time.time()
                     response = chain.invoke({"text": text, "image": image_b64})
                     query_end_time = time.time()
+                    timing["query"] = query_end_time - query_start_time
+
                     logger.info(f"Generated response: {response}")
                     # sample_rate, audio_array = text_to_speech.synthesize_long_text(
                     #     response
@@ -69,12 +83,14 @@ def main(chain, image_b64: str, console: Console, speech_to_text: whisper.Whispe
                 console.print(
                     response, overflow="fold"
                 )  # Folding of output on overflow.
-                console.rule()
-                console.print(
-                    ":clock8:",
-                    f"Query took {query_end_time - query_start_time:.2f}s.",
-                    sep=" ",
-                )
+                if timings:
+                    console.rule("[cyan]Timings")
+                    console.print(
+                        ":clock8:",
+                        f"Transcription in {timing['transcribe']:.2f}s.",
+                        f"Query in {timing['query']:.2f}s.",
+                        sep="",
+                    )
                 # play_audio(sample_rate, audio_array)
             else:
                 console.print(
@@ -102,12 +118,16 @@ if __name__ == "__main__":
     image_b64 = prep_image(image_path=file_path)
 
     # Initialize the model and parser.
-    model = ChatOllama(model="llava-phi3")
+    model = ChatOllama(model="llama3.2:1b")
     parser = StrOutputParser()
 
     # Create a chain of functions to process the query.
     chain = get_prompt | model | parser
 
     main(
-        chain=chain, image_b64=image_b64, console=console, speech_to_text=speech_to_text
+        chain=chain,
+        image_b64=image_b64,
+        console=console,
+        speech_to_text=speech_to_text,
+        timings=True,
     )
